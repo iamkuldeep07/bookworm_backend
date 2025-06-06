76a02f3a551be1a509f8015de260b3892f3998f6
iamkuldeep07 <kuldeep45.kd@gmail.com>
2025-06-06 14:14:14 +0530

    Update sendToken.js

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,5 +1,6 @@
 export const sendToken = (user, statusCode, message, res) => {
   const token = user.generateToken();
+
   res
     .status(statusCode)
     .cookie("token", token, {
@@ -7,6 +8,8 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      secure: true,         // ✅ required for HTTPS
+      sameSite: "None",     // ✅ required for cross-site cookies
     })
     .json({
       success: true,
