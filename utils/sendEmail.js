cc1533e514491e251bbab5cf10936a286442d4f3
iamkuldeep07 <kuldeep45.kd@gmail.com>
2026-04-08 18:31:31 +0530

    nodemailer fix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,20 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export const sendEmail = async ({ email, subject, message }) => {
-    await resend.emails.send({
-        from: "onboarding@resend.dev",
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.SMTP_MAIL,
+            pass: process.env.SMTP_PASSWORD, // App password
+        },
+    });
+
+    const mailOptions = {
+        from: process.env.SMTP_MAIL,
         to: email,
         subject,
         html: message,
-    });
+    };
+
+    await transporter.sendMail(mailOptions);
 };
